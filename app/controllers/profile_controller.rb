fe0a4e1000a4f817f1e2db3dae2e66c0da05e1ea
jay2452 <jay2452@live.com>
2015-06-11 19:46:24 +0530

    sbcjef

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,7 +2,7 @@ class ProfileController < ApplicationController
 	before_action :authenticate_user!
   	
   	def show
-  		@user = User.find(params[:id])
+  		@user = current_user#User.find(params[:id])
   		@articles = @user.articles.paginate(page: params[:page])
   	end
 end
