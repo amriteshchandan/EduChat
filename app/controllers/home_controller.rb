3e1804acb845d44345e42c47edf20de02ea8ce34
jay2452 <jay2452@live.com>
2015-06-11 17:00:13 +0530

    article changed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+  	@article = current_user.articles.build if user_signed_in?
   	if user_signed_in?
   		if current_user.sign_in_count < 1
   			redirect_to edit_user_registration_path
--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,7 +2,7 @@ class ProfileController < ApplicationController
 	before_action :authenticate_user!
   	
   	def show
-  		@user = current_user
+  		@user = User.find(params[:id])
   		@articles = @user.articles.paginate(page: params[:page])
   	end
 end
