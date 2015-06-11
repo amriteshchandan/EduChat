955eeef9f5e00cb72e3addff2c184e481d82789b
jay2452 <jay2452@live.com>
2015-06-11 19:31:30 +0530

    sbcj

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,12 @@ class HomeController < ApplicationController
   def index
   	@articles = Article.all
   	@article = current_user.articles.build if user_signed_in?
-  	@feed_items = current_user.feed.paginate(page: params[:page])
+
+  	if current_user.present?
+  		@feed_items = current_user.feed.paginate(page: params[:page])
+  	else
+  		render 'home/index'
+  	end	
 
   	if user_signed_in?
   		if current_user.sign_in_count < 1
