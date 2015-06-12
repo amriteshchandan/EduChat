b87f93e806935d4e7ff8672f5d4d9bbaf35408d2
jay2452 <jay2452@live.com>
2015-06-12 10:47:04 +0530

    dfvdsv

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,20 +1,18 @@
 class HomeController < ApplicationController
+  @@a = 1 ;
   def index
   	@articles = Article.all
-  	#@article = current_user.articles.build if user_signed_in?
-
-  	#if current_user.present?
-  	#	@feed_items = current_user.feed.paginate(page: params[:page])
-  	#else
-  		
-  	#end	
 
   	if user_signed_in?
       @article = current_user.articles.build
       @feed_items = current_user.feed.paginate(page: params[:page])
   		
-      if current_user.sign_in_count <= 1
+      if @@a > 1
+          render 'home/index'
+      elsif current_user.sign_in_count <= 1
+        @@a = @@a + 1
   			redirect_to edit_user_registration_path
+
   		end
     else
       render 'home/index'
