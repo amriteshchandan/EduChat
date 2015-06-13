58532806ae830baef0655f3511d854efb8cf1b9e
jay2452 <jay2452@live.com>
2015-06-13 10:03:37 +0530

    profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -3,7 +3,9 @@ class ProfileController < ApplicationController
   	
   	def show
   		@technologies = current_user.technologies
-  		@user = current_user
+  		@interested_tech = current_user.user_technologies.where("technology_type = ?", 'interested')
+  		@known_tech = current_user.user_technologies.where("technology_type = ?", 'known')
+
   		@user = current_user#User.find(params[:id])
   		@articles = @user.articles.paginate(page: params[:page])
   	end
