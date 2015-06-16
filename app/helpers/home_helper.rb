6419f75c7822a279cab1a83a571542ce29ff621d
jay2452 <jay2452@live.com>
2015-06-16 15:31:49 +0530

    df

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,7 +1,7 @@
 module HomeHelper
 	def feed_render
 		arr = []
-		@feed_items = current_user.feed.paginate(page: params[:page], per_page: 5)
+		@feed_items = current_user.feed#.paginate(page: params[:page], per_page: 5)
 		@feed_items.each do |f|
 			arr << f[:title]
 		end
@@ -11,12 +11,13 @@ module HomeHelper
 
 	def user_render
 		arr1 = []
-		@may_know_user = current_user.user_may_know_other_user.paginate(page: params[:page], per_page: 5)
+		@may_know_user = current_user.user_may_know_other_user#.paginate(page: params[:page], per_page: 5)
 		if @may_know_user != nil
 			@may_know_user.each do |u|
 				arr1 << u[:username]
 			end
 		end
+		arr1.delete_if { |a| a == current_user.username }
 		return arr1
 	end
 end
